4c7fdd415db2ff61c7663b90409a62cf5cc69d0b
SolovyovAA <solovyov.aa.prog@gmail.com>
2020-04-11 00:15:32 +0300

    HelloWorld: Variables

--- HelloWorld/hello_world.py
+++ HelloWorld/hello_world.py
@@ -10,3 +10,16 @@ print(2/2)
 print(2/2) #Деление с округлением (до меньшего а-ля Py2)
 print(2%2)
 print(2&2)
+
+#3 Lesson 3: Variables and PEP8 conv.
+# x - int
+x = 5
+print(x)
+
+# x - let's change x var type to str (cuz Py has dynamically typed variables)
+x = "Some string"
+print(x)
+
+# In Py name of variables should be in snake_case
+type_of_variable_x = type( x )
+print(type_of_variable_x)
