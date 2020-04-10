b4fb869baa2e89b3bfdcd44fc403f73693f0788c
SolovyovAA <solovyov.aa.prog@gmail.com>
2020-04-11 00:00:56 +0300

    HelloWorld: Added math ops

--- HelloWorld/HelloWorld.py
+++ HelloWorld/HelloWorld.py
@@ -1,1 +1,12 @@
+#1 Lesson 1: Hello World
 print("Hello Python!")
+
+#2 Lesson 2: Math ops
+print(2+2)
+print(2-2)
+print(2*2)
+print(2**3) #Степень
+print(2/2)
+print(2/2) #Деление с округлением (до меньшего а-ля Py2)
+print(2%2)
+print(2&2)
